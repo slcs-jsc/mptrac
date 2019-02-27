1b715bb33889382336c0262fda74eceb8d9b64d7
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2019-02-27 16:47:48 +0100

    Added filters to atm_stat tool.

--- src/atm_stat.c
+++ src/atm_stat.c
@@ -30,18 +30,20 @@ int main(
 
   ctl_t ctl;
 
-  atm_t *atm;
+  atm_t *atm, *atm_filt;
 
   FILE *out;
 
   char tstr[LEN];
 
-  double latm, lonm, t, qm[NQ], *work, zm, *zs;
+  double lat0, lat1, latm, lon0, lon1, lonm, p0, p1,
+    t, t0, qm[NQ], *work, zm, *zs;
 
   int f, ip, iq, year, mon, day, hour, min;
 
   /* Allocate... */
   ALLOC(atm, atm_t, 1);
+  ALLOC(atm_filt, atm_t, 1);
   ALLOC(work, double,
 	NP);
   ALLOC(zs, double,
@@ -53,6 +55,12 @@ int main(
 
   /* Read control parameters... */
   read_ctl(argv[1], argc, argv, &ctl);
+  p0 = P(scan_ctl(argv[1], argc, argv, "STAT_Z0", -1, "-1000", NULL));
+  p1 = P(scan_ctl(argv[1], argc, argv, "STAT_Z1", -1, "1000", NULL));
+  lat0 = scan_ctl(argv[1], argc, argv, "STAT_LAT0", -1, "-1000", NULL);
+  lat1 = scan_ctl(argv[1], argc, argv, "STAT_LAT1", -1, "1000", NULL);
+  lon0 = scan_ctl(argv[1], argc, argv, "STAT_LON0", -1, "-1000", NULL);
+  lon1 = scan_ctl(argv[1], argc, argv, "STAT_LON1", -1, "1000", NULL);
 
   /* Write info... */
   printf("Write air parcel statistics: %s\n", argv[2]);
@@ -64,13 +72,14 @@ int main(
   /* Write header... */
   fprintf(out,
 	  "# $1 = time [s]\n"
-	  "# $2 = altitude (%s) [km]\n"
-	  "# $3 = longitude (%s) [deg]\n"
-	  "# $4 = latitude (%s) [deg]\n", argv[3], argv[3], argv[3]);
+	  "# $2 = trajectory time [s]\n"
+	  "# $3 = altitude (%s) [km]\n"
+	  "# $4 = longitude (%s) [deg]\n"
+	  "# $5 = latitude (%s) [deg]\n", argv[3], argv[3], argv[3]);
   for (iq = 0; iq < ctl.nq; iq++)
-    fprintf(out, "# $%d = %s (%s) [%s]\n", iq + 5,
+    fprintf(out, "# $%d = %s (%s) [%s]\n", iq + 6,
 	    ctl.qnt_name[iq], argv[3], ctl.qnt_unit[iq]);
-  fprintf(out, "# $%d = number of particles\n\n", ctl.nq + 5);
+  fprintf(out, "# $%d = number of particles\n\n", ctl.nq + 6);
 
   /* Loop over files... */
   for (f = 4; f < argc; f++) {
@@ -91,75 +100,105 @@ int main(
     min = atoi(tstr);
     time2jsec(year, mon, day, hour, min, 0, 0, &t);
 
+    /* Save intial time... */
+    if (f == 4)
+      t0 = t;
+
+    /* Filter data... */
+    atm_filt->np = 0;
+    for (ip = 0; ip < atm->np; ip++) {
+
+      /* Check time... */
+      if (!gsl_finite(atm->time[ip]))
+	continue;
+
+      /* Check spatial range... */
+      if (atm->p[ip] > p0 || atm->p[ip] < p1
+	  || atm->lon[ip] < lon0 || atm->lon[ip] > lon1
+	  || atm->lat[ip] < lat0 || atm->lat[ip] > lat1)
+	continue;
+
+      /* Save data... */
+      atm_filt->time[atm_filt->np] = atm->time[ip];
+      atm_filt->p[atm_filt->np] = atm->p[ip];
+      atm_filt->lon[atm_filt->np] = atm->lon[ip];
+      atm_filt->lat[atm_filt->np] = atm->lat[ip];
+      for (iq = 0; iq < ctl.nq; iq++)
+	atm_filt->q[iq][atm_filt->np] = atm->q[iq][ip];
+      atm_filt->np++;
+    }
+
     /* Get heights... */
-    for (ip = 0; ip < atm->np; ip++)
-      zs[ip] = Z(atm->p[ip]);
+    for (ip = 0; ip < atm_filt->np; ip++)
+      zs[ip] = Z(atm_filt->p[ip]);
 
     /* Get statistics... */
     if (strcasecmp(argv[3], "mean") == 0) {
-      zm = gsl_stats_mean(zs, 1, (size_t) atm->np);
-      lonm = gsl_stats_mean(atm->lon, 1, (size_t) atm->np);
-      latm = gsl_stats_mean(atm->lat, 1, (size_t) atm->np);
+      zm = gsl_stats_mean(zs, 1, (size_t) atm_filt->np);
+      lonm = gsl_stats_mean(atm_filt->lon, 1, (size_t) atm_filt->np);
+      latm = gsl_stats_mean(atm_filt->lat, 1, (size_t) atm_filt->np);
       for (iq = 0; iq < ctl.nq; iq++)
-	qm[iq] = gsl_stats_mean(atm->q[iq], 1, (size_t) atm->np);
+	qm[iq] = gsl_stats_mean(atm_filt->q[iq], 1, (size_t) atm_filt->np);
     } else if (strcasecmp(argv[3], "stddev") == 0) {
-      zm = gsl_stats_sd(zs, 1, (size_t) atm->np);
-      lonm = gsl_stats_sd(atm->lon, 1, (size_t) atm->np);
-      latm = gsl_stats_sd(atm->lat, 1, (size_t) atm->np);
+      zm = gsl_stats_sd(zs, 1, (size_t) atm_filt->np);
+      lonm = gsl_stats_sd(atm_filt->lon, 1, (size_t) atm_filt->np);
+      latm = gsl_stats_sd(atm_filt->lat, 1, (size_t) atm_filt->np);
       for (iq = 0; iq < ctl.nq; iq++)
-	qm[iq] = gsl_stats_sd(atm->q[iq], 1, (size_t) atm->np);
+	qm[iq] = gsl_stats_sd(atm_filt->q[iq], 1, (size_t) atm_filt->np);
     } else if (strcasecmp(argv[3], "min") == 0) {
-      zm = gsl_stats_min(zs, 1, (size_t) atm->np);
-      lonm = gsl_stats_min(atm->lon, 1, (size_t) atm->np);
-      latm = gsl_stats_min(atm->lat, 1, (size_t) atm->np);
+      zm = gsl_stats_min(zs, 1, (size_t) atm_filt->np);
+      lonm = gsl_stats_min(atm_filt->lon, 1, (size_t) atm_filt->np);
+      latm = gsl_stats_min(atm_filt->lat, 1, (size_t) atm_filt->np);
       for (iq = 0; iq < ctl.nq; iq++)
-	qm[iq] = gsl_stats_min(atm->q[iq], 1, (size_t) atm->np);
+	qm[iq] = gsl_stats_min(atm_filt->q[iq], 1, (size_t) atm_filt->np);
     } else if (strcasecmp(argv[3], "max") == 0) {
-      zm = gsl_stats_max(zs, 1, (size_t) atm->np);
-      lonm = gsl_stats_max(atm->lon, 1, (size_t) atm->np);
-      latm = gsl_stats_max(atm->lat, 1, (size_t) atm->np);
+      zm = gsl_stats_max(zs, 1, (size_t) atm_filt->np);
+      lonm = gsl_stats_max(atm_filt->lon, 1, (size_t) atm_filt->np);
+      latm = gsl_stats_max(atm_filt->lat, 1, (size_t) atm_filt->np);
       for (iq = 0; iq < ctl.nq; iq++)
-	qm[iq] = gsl_stats_max(atm->q[iq], 1, (size_t) atm->np);
+	qm[iq] = gsl_stats_max(atm_filt->q[iq], 1, (size_t) atm_filt->np);
     } else if (strcasecmp(argv[3], "skew") == 0) {
-      zm = gsl_stats_skew(zs, 1, (size_t) atm->np);
-      lonm = gsl_stats_skew(atm->lon, 1, (size_t) atm->np);
-      latm = gsl_stats_skew(atm->lat, 1, (size_t) atm->np);
+      zm = gsl_stats_skew(zs, 1, (size_t) atm_filt->np);
+      lonm = gsl_stats_skew(atm_filt->lon, 1, (size_t) atm_filt->np);
+      latm = gsl_stats_skew(atm_filt->lat, 1, (size_t) atm_filt->np);
       for (iq = 0; iq < ctl.nq; iq++)
-	qm[iq] = gsl_stats_skew(atm->q[iq], 1, (size_t) atm->np);
+	qm[iq] = gsl_stats_skew(atm_filt->q[iq], 1, (size_t) atm_filt->np);
     } else if (strcasecmp(argv[3], "kurt") == 0) {
-      zm = gsl_stats_kurtosis(zs, 1, (size_t) atm->np);
-      lonm = gsl_stats_kurtosis(atm->lon, 1, (size_t) atm->np);
-      latm = gsl_stats_kurtosis(atm->lat, 1, (size_t) atm->np);
+      zm = gsl_stats_kurtosis(zs, 1, (size_t) atm_filt->np);
+      lonm = gsl_stats_kurtosis(atm_filt->lon, 1, (size_t) atm_filt->np);
+      latm = gsl_stats_kurtosis(atm_filt->lat, 1, (size_t) atm_filt->np);
       for (iq = 0; iq < ctl.nq; iq++)
-	qm[iq] = gsl_stats_kurtosis(atm->q[iq], 1, (size_t) atm->np);
+	qm[iq] =
+	  gsl_stats_kurtosis(atm_filt->q[iq], 1, (size_t) atm_filt->np);
     } else if (strcasecmp(argv[3], "median") == 0) {
-      zm = gsl_stats_median(zs, 1, (size_t) atm->np);
-      lonm = gsl_stats_median(atm->lon, 1, (size_t) atm->np);
-      latm = gsl_stats_median(atm->lat, 1, (size_t) atm->np);
+      zm = gsl_stats_median(zs, 1, (size_t) atm_filt->np);
+      lonm = gsl_stats_median(atm_filt->lon, 1, (size_t) atm_filt->np);
+      latm = gsl_stats_median(atm_filt->lat, 1, (size_t) atm_filt->np);
       for (iq = 0; iq < ctl.nq; iq++)
-	qm[iq] = gsl_stats_median(atm->q[iq], 1, (size_t) atm->np);
+	qm[iq] = gsl_stats_median(atm_filt->q[iq], 1, (size_t) atm_filt->np);
     } else if (strcasecmp(argv[3], "absdev") == 0) {
-      zm = gsl_stats_absdev(zs, 1, (size_t) atm->np);
-      lonm = gsl_stats_absdev(atm->lon, 1, (size_t) atm->np);
-      latm = gsl_stats_absdev(atm->lat, 1, (size_t) atm->np);
+      zm = gsl_stats_absdev(zs, 1, (size_t) atm_filt->np);
+      lonm = gsl_stats_absdev(atm_filt->lon, 1, (size_t) atm_filt->np);
+      latm = gsl_stats_absdev(atm_filt->lat, 1, (size_t) atm_filt->np);
       for (iq = 0; iq < ctl.nq; iq++)
-	qm[iq] = gsl_stats_absdev(atm->q[iq], 1, (size_t) atm->np);
+	qm[iq] = gsl_stats_absdev(atm_filt->q[iq], 1, (size_t) atm_filt->np);
     } else if (strcasecmp(argv[3], "mad") == 0) {
-      zm = gsl_stats_mad0(zs, 1, (size_t) atm->np, work);
-      lonm = gsl_stats_mad0(atm->lon, 1, (size_t) atm->np, work);
-      latm = gsl_stats_mad0(atm->lat, 1, (size_t) atm->np, work);
+      zm = gsl_stats_mad0(zs, 1, (size_t) atm_filt->np, work);
+      lonm = gsl_stats_mad0(atm_filt->lon, 1, (size_t) atm_filt->np, work);
+      latm = gsl_stats_mad0(atm_filt->lat, 1, (size_t) atm_filt->np, work);
       for (iq = 0; iq < ctl.nq; iq++)
-	qm[iq] = gsl_stats_mad0(atm->q[iq], 1, (size_t) atm->np, work);
+	qm[iq] =
+	  gsl_stats_mad0(atm_filt->q[iq], 1, (size_t) atm_filt->np, work);
     } else
       ERRMSG("Unknown parameter!");
 
     /* Write data... */
-    fprintf(out, "%.2f %g %g %g", t, zm, lonm, latm);
+    fprintf(out, "%.2f %.2f %g %g %g", t, t - t0, zm, lonm, latm);
     for (iq = 0; iq < ctl.nq; iq++) {
       fprintf(out, " ");
       fprintf(out, ctl.qnt_format[iq], qm[iq]);
     }
-    fprintf(out, " %d\n", atm->np);
+    fprintf(out, " %d\n", atm_filt->np);
   }
 
   /* Close file... */
@@ -167,6 +206,7 @@ int main(
 
   /* Free... */
   free(atm);
+  free(atm_filt);
   free(work);
   free(zs);
 
