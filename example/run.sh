30e9e332c769fb3435dff9290e7f3ab5123d9ea4
Sebastian Keller <sebastian.f.keller@gmail.com>
2019-04-10 13:23:58 +0200

    reenabled turbulence / diffusion

--- example/run.sh
+++ example/run.sh
@@ -5,7 +5,8 @@ rm -rf data plots && mkdir -p data plots
 
 # Set time range of simulations...
 t0=$(../src/time2jsec 2011 6 5 0 0 0 0)
-t1=$(../src/time2jsec 2011 6 5 1 0 0 0)
+#t1=$(../src/time2jsec 2011 6 5 1 0 0 0)
+t1=$(../src/time2jsec 2011 6 8 0 0 0 0)
 
 # Add "mass" to air parcel quantities...
 qnt="NQ 1 QNT_NAME[0] m"
@@ -20,30 +21,30 @@ qnt="NQ 1 QNT_NAME[0] m"
 # Split into 10000 air parcels with a total mass of 10^9 kg
 # and scatter around initial position...
 ../src/atm_split - data/atm_init.tab data/atm_split.tab $qnt \
-		 SPLIT_N 10000 SPLIT_M 1e9 SPLIT_DX 30.0 SPLIT_DZ 1.0
+		 SPLIT_N 9999 SPLIT_M 1e9 SPLIT_DX 30.0 SPLIT_DZ 1.0
 
 # Calculate trajectories, write air parcel and grid output every 12 h...
 echo "data" > data/dirlist
-../src/trac data/dirlist - atm_split.tab meteo/ei DT_MET 259200 $qnt \
-	    T_STOP $t1 DIRECTION 1 \
-	    ATM_BASENAME atm ATM_DT_OUT 600 \
-	    GRID_BASENAME grid GRID_DT_OUT 600 \
-	    GRID_LON0 -100 GRID_LON1 40 \
-	    GRID_LAT0 -65 GRID_LAT1 -20 \
-	    GRID_NX 280 GRID_NY 90 \
-            TURB_DX_TROP 0 \
-            TURB_DX_STRAT 0 \
-            TURB_DZ_TROP 0 \
-            TURB_DZ_STRAT 0 \
-            TURB_MESOX 0 \
-            TURB_MESOZ 0 
 #../src/trac data/dirlist - atm_split.tab meteo/ei DT_MET 259200 $qnt \
 #	    T_STOP $t1 DIRECTION 1 \
-#	    ATM_BASENAME atm ATM_DT_OUT 43200 \
-#	    GRID_BASENAME grid GRID_DT_OUT 43200 \
+#	    ATM_BASENAME atm ATM_DT_OUT 600 \
+#	    GRID_BASENAME grid GRID_DT_OUT 600 \
 #	    GRID_LON0 -100 GRID_LON1 40 \
 #	    GRID_LAT0 -65 GRID_LAT1 -20 \
-#	    GRID_NX 280 GRID_NY 90
+#	    GRID_NX 280 GRID_NY 90 \
+#            TURB_DX_TROP 0 \
+#            TURB_DX_STRAT 0 \
+#            TURB_DZ_TROP 0 \
+#            TURB_DZ_STRAT 0 \
+#            TURB_MESOX 0 \
+#            TURB_MESOZ 0 
+../src/trac data/dirlist - atm_split.tab meteo/ei DT_MET 259200 $qnt \
+	    T_STOP $t1 DIRECTION 1 \
+	    ATM_BASENAME atm ATM_DT_OUT 43200 \
+	    GRID_BASENAME grid GRID_DT_OUT 43200 \
+	    GRID_LON0 -100 GRID_LON1 40 \
+	    GRID_LAT0 -65 GRID_LAT1 -20 \
+	    GRID_NX 280 GRID_NY 90
 
 # Use gnuplot to plot air parcels...
 for f in $(ls data/atm_2011*tab) ; do
