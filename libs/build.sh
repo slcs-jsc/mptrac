f30cebcfb4119adf4a0dfd2bb6b3d5e23be5bd4e
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2021-12-06 08:17:35 +0100

    Update to HDF5 v1.12.1 and netCDF v4.8.1.

--- libs/build.sh
+++ libs/build.sh
@@ -12,9 +12,9 @@ threads=$(cat /proc/cpuinfo | grep processor | wc -l)
 
 # Prepare directories...
 mkdir -p $target/src $target/bin $target/lib $target/man/man1 \
-    && cp *tar.gz $target/src \
+    && cp *tar.bz2 $target/src \
     && cd $target/src \
-    && for f in $(ls *tar.gz) ; do tar xvzf $f ; done \
+    && for f in $(ls *tar.bz2) ; do tar xvjf $f ; done \
 	|| exit
 
 # GSL...
@@ -35,7 +35,7 @@ if [ $1 = "nc4" ] ; then
 	    || exit
     
     # HDF5...
-    dir=hdf5-1.12.0
+    dir=hdf5-1.12.1
     cd $target/src/$dir \
 	&& ./configure --prefix=$target --with-zlib=$target --enable-hl \
 	&& make -j$threads ; make -j$threads && make check && make install && make clean \
@@ -43,8 +43,14 @@ if [ $1 = "nc4" ] ; then
 fi
 
 # netCDF...
-dir=netcdf-c-4.7.4
+dir=netcdf-c-4.8.1
 cd $target/src/$dir \
     && CPPFLAGS=-I$target/include LDFLAGS=-L$target/lib ./configure --prefix=$target --disable-dap $([ $1 != "nc4" ] && echo "--disable-netcdf-4") \
     && make -j$threads && make check && make install && make clean \
 	|| exit
+
+# Summary...
+echo -e "\n***** gsl-config *****\n"
+./build/bin/gsl-config --libs --cflags --version
+echo -e "\n***** nc-config *****"
+./build/bin/nc-config --all
