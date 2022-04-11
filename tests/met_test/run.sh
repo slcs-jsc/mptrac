e1892ed19b682f8750a53fb5e2fa815ae3dded79
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2022-04-11 23:14:54 +0200

    Renamed tests.

--- tests/met_test/run.sh
+++ tests/met_test/run.sh
@@ -11,21 +11,21 @@ trac=../../src
 rm -rf data && mkdir -p data
 
 # Sample map, profile, zonal mean, and spectral data...
-$trac/met_map - data/map.tab ../meteo/ei_2011_06_05_00.nc \
+$trac/met_map - data/map.tab ../data/ei_2011_06_05_00.nc \
 	      MAP_LON0 -180 MAP_LON1 180 MAP_DLON 6 \
 	      MAP_LAT0 -90 MAP_LAT1 90 MAP_DLAT 4 \
 	      MAP_THETA 380 TROPO 3
-$trac/met_prof - data/prof.tab ../meteo/ei_2011_06_05_00.nc \
+$trac/met_prof - data/prof.tab ../data/ei_2011_06_05_00.nc \
 	       PROF_Z0 0 PROF_Z1 60 PROF_DZ 0.1 \
 	       PROF_LON0 20 PROF_LON1 30 PROF_DLON 1 \
 	       PROF_LAT0 40 PROF_LAT1 50 PROD_DLAT 1 \
 	       TROPO 4
-$trac/met_zm - data/zm.tab ../meteo/ei_2011_06_05_00.nc \
+$trac/met_zm - data/zm.tab ../data/ei_2011_06_05_00.nc \
 	     ZM_Z0 0 ZM_Z1 60 ZM_DZ 1 \
 	     ZM_LON0 -180 ZM_LON1 180 ZM_DLON 1 \
 	     ZM_LAT0 -90 ZM_LAT1 90 ZM_DLAT 3 \
 	     TROPO 5
-$trac/met_spec - data/spec.tab ../meteo/ei_2011_06_05_00.nc \
+$trac/met_spec - data/spec.tab ../data/ei_2011_06_05_00.nc \
 	       SPEC_WAVEMAX 3
 
 # Create init file for sampling...
@@ -38,7 +38,7 @@ $trac/atm_init - data/atm_init.tab \
 
 # Sample meteo data...
 $trac/met_sample - data/sample.tab data/atm_init.tab \
-		 METBASE ../meteo/ei DT_MET 86400.0
+		 METBASE ../data/ei DT_MET 86400.0
 
 # Compare files...
 echo -e "\nCompare results..."
