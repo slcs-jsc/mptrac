dcf6c72cb73fd647d3f2cba605850319668d5e5c
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2022-07-17 21:14:42 +0200

    Added GOZCARDS climatology data files.

--- src/libtrac.c
+++ src/libtrac.c
@@ -2268,7 +2268,7 @@ void read_ctl(
   ctl->oh_chem_beta =
     scan_ctl(filename, argc, argv, "OH_CHEM_BETA", -1, "0", NULL);
   scan_ctl(filename, argc, argv, "CLIM_OH_FILENAME", -1,
-	   "../../data/radical_species_climatology.nc",
+	   "../../data/clams_radical_species.nc",
 	   ctl->clim_oh_filename);
   ctl->tdec_trop = scan_ctl(filename, argc, argv, "TDEC_TROP", -1, "0", NULL);
   ctl->tdec_strat =
