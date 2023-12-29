7eae248608e089182ce1de1c1df4d04c10502cca
laomangio <liumzh5@outlook.com>
2023-12-29 15:00:56 +0100

    Update build script of kpp

--- projects/kpp/build_KPP.sh
+++ projects/kpp/build_KPP.sh
@@ -8,21 +8,23 @@ fi
 
 # Set directory...
 kppdir=$1
+REBUILD=1
 
-# Copy Makefile and header file...
-cp ./Makefile $kppdir/
-cp ./kpp_chem.h $kppdir/
+
 
 # Set target directory...
-target=$(cd ../../libs/build && pwd)
+target=$(cd ../libs/build && pwd)
 export KPP_HOME=$target/src/KPP
 export KPP_FLEX_LIB_DIR=$target/src/KPP
 
-# Build KPP code...
-cd $kppdir  
-$KPP_HOME/bin/kpp chem.kpp \
-    && make clean \
-    && make lib INFO=0 \
-    && cp libkpp.a $target/lib \
-    && cp *.h $target/include \
-    && rm *.o
+# Build KPP code... 
+cd $kppdir
+[ $REBUILD -eq 1 ] && cp ../Makefile ./ \
+&& cp ../kpp_chem.h ./ \
+&& $KPP_HOME/bin/kpp chem.kpp 
+
+make clean \
+&& make lib INFO=0 \
+&& cp libkpp.a $target/lib \
+&& cp *.h $target/include \
+&& rm *.o
