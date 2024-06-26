82bcf1244082629b9a8d3fee4318f0f436104a0e
ye.lu <ye.lu@fz-juelich.de>
2024-06-26 09:48:48 +0200

    new configuration

---
+++ config/test_cpu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+strFolderIn=$1
+str_in=`uname -a`
+if_found=.false.
+for i in $str_in ; do 
+    if [[ $i  == *"juwels"* ]] ; then
+        printf "we are on juwels\n"
+        printf "`uname -a`\n"
+	printf "PWD: `pwd`\n"
+	printf "Loading Modules\n"
+        module load Intel ParaStationMPI netCDF GSL
+	printf "Compiling MPTRAC using modules on JUWELS ... \n "
+	cd ./src
+        make check	
+        if_found=.true. 
+
+    elif [[ $i == *"jureca"* ]] ; then
+        printf "we are on jureca\n"
+        printf "`uname -a`\n"
+	printf "PWD: `pwd`\n"
+	printf "Loading Modules\n"
+        module load Intel ParaStationMPI netCDF GSL
+	printf "Compiling MPTRAC using modules on JURECA ... \n "
+	cd ./src
+        make check	
+        if_found=.true. 
+    else
+	printf "None of the machine matches\n" 
+    fi 
+done
+
+if [[ ! $if_found  ]]; then 
+    printf "None of the system matches the configuration \n"
+    printf "please check that if your machine is registered on the buildbot master\n"	
+fi
