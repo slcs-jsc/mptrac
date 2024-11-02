c242b79e61b2e54890e5730f23451269ca0a4875
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2024-11-02 07:15:41 +0100

    Fix GFS retrieval script.

--- projects/meteo/get_gfs.sh
+++ projects/meteo/get_gfs.sh
@@ -50,7 +50,7 @@ for fh in $(seq 0 3 240 | awk '{printf(" %03d", $1)}') ; do
     wget -nv -O "$dir"/tmp.grib2 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p50.pl?file=gfs.t00z.pgrb2full.0p50.f'"$fh"'&&lev_0.01_mb=on&lev_0.02_mb=on&lev_0.04_mb=on&lev_0.07_mb=on&lev_0.1_mb=on&lev_0.2_mb=on&lev_0.4_mb=on&lev_0.7_mb=on&lev_1_mb=on&lev_2_mb=on&lev_3_mb=on&lev_5_mb=on&lev_7_mb=on&lev_10_mb=on&lev_15_mb=on&lev_20_mb=on&lev_30_mb=on&lev_40_mb=on&lev_50_mb=on&lev_70_mb=on&lev_100_mb=on&lev_150_mb=on&lev_200_mb=on&lev_250_mb=on&lev_300_mb=on&lev_350_mb=on&lev_400_mb=on&lev_450_mb=on&lev_500_mb=on&lev_550_mb=on&lev_600_mb=on&lev_650_mb=on&lev_700_mb=on&lev_750_mb=on&lev_800_mb=on&lev_850_mb=on&lev_900_mb=on&lev_925_mb=on&lev_950_mb=on&lev_975_mb=on&lev_1000_mb=on&lev_surface=on&var_TMP=on&var_UGRD=on&var_VGRD=on&var_VVEL=on&var_SPFH=on&var_O3MR=on&var_PRES=on&var_HGT=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.'"$tstr"'%2Fatmos' || continue
     
     # Set netCDF output filename...
-    out=$($trac/jsec2time "$($trac/time2jsec "$year $mon $day" 0 0 0 0 | awk -v fh="$fh" '{print $1+3600*fh}')" | awk '{printf("gfs_%d_%02d_%02d_%02d.nc", $1, $2, $3, $4)}')
+    out=$($trac/jsec2time "$($trac/time2jsec $year $mon $day 0 0 0 0 | awk -v fh="$fh" '{print $1+3600*fh}')" | awk '{printf("gfs_%d_%02d_%02d_%02d.nc", $1, $2, $3, $4)}')
     
     # Convert from grib to netCDF...
     cdo -f nc copy "$dir"/tmp.grib2 "$dir"/tmp.nc || exit
