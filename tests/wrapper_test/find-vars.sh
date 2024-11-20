8f9786f5248f391e973fa302c60cb1c221d82737
Catrin <cat.meyer@fz-juelich.de>
2024-11-20 05:55:56 +0100

    Include structure atm_t in comparison between Fortran and C

--- tests/wrapper_test/find-vars.sh
+++ tests/wrapper_test/find-vars.sh
@@ -12,6 +12,7 @@ awk '$0=="  TYPE, bind(c) :: clim_t"{f=1;next} /END TYPE clim_t/{f=0} f' < ../..
 awk '/TYPE, bind\(c\) :: clim_zm_t/{f=1;next} /END TYPE clim_zm_t/{f=0} f' < ../../src/mptrac_fortran.f90 > data/test_clim_zm_f.asc
 awk '/TYPE, bind\(c\) :: clim_ts_t/{f=1;next} /END TYPE clim_ts_t/{f=0} f' < ../../src/mptrac_fortran.f90 > data/test_clim_ts_f.asc
 awk '/TYPE, bind\(c\) :: clim_photo_t/{f=1;next} /END TYPE clim_photo_t/{f=0} f' < ../../src/mptrac_fortran.f90 > data/test_clim_photo_f.asc
+awk '/TYPE, bind\(c\) :: atm_t/{f=1;next} /END TYPE atm_t/{f=0} f' < ../../src/mptrac_fortran.f90 > data/test_atm_f.asc
 
 awk '/Control parameters/{f=1;next} /\} ctl_t;/{f=0} f' < ../../src/mptrac.h > data/test_ctl_c.asc
 awk '/Meteo data structure/{f=1;next} /\} met_t;/{f=0} f' < ../../src/mptrac.h > data/test_met_c.asc
@@ -19,6 +20,7 @@ awk '/climatological data containing/{f=1;next} /\} clim_t;/{f=0} f' < ../../src
 awk '/Climatological data in the form of zonal means./{f=1;next} /\} clim_zm_t;/{f=0} f' < ../../src/mptrac.h > data/test_clim_zm_c.asc
 awk '/Climatological data in the form of time series./{f=1;next} /\} clim_ts_t;/{f=0} f' < ../../src/mptrac.h > data/test_clim_ts_c.asc
 awk '/Climatological data in the form of photolysis rates./{f=1;next} /\} clim_photo_t;/{f=0} f' < ../../src/mptrac.h > data/test_clim_photo_c.asc
+awk '/Air parcel data./{f=1;next} /\} atm_t;/{f=0} f' < ../../src/mptrac.h > data/test_atm_c.asc
 
 # compare number of structure entries and variables inside 
 grep ";" data/test_ctl_c.asc | wc -l > x
@@ -175,12 +177,39 @@ if cmp -s x y && cmp -s data/var_clim_photo_c.asc data/var_clim_photo_f.asc ; th
     cat x
     rm x y
 else
-    echo "Number of climatological datain the form of photolysis rates are not equal. C has: "
+    echo "Number of climatological data in the form of photolysis rates are not equal. C has: "
     cat x
     echo "and Fortran has: "
     cat y
     echo "Difference between var_clim_photo_c.asc and var_clim_photo_f.asc"
     diff data/var_clim_photo_c.asc data/var_clim_photo_f.asc
+    rm x y
+    exit_code=99
+    exit $exit_code
+fi
+
+grep ";" data/test_clim_photo_c.asc | wc -l > x
+wc -l < data/test_clim_photo_f.asc > y
+for varc in $(grep ";" data/test_atm_c.asc| awk '{print $2}')
+do
+    echo ${varc}|awk -F'[;[]' '{print $1}' >> data/var_atm_c.asc
+done
+
+for varf in $(grep "::" data/test_atm_f.asc|awk '{print $NF}')
+do
+    echo $varf >> data/var_atm_f.asc
+done
+if cmp -s x y && cmp -s data/var_atm_c.asc data/var_atm_f.asc ; then
+    echo "Number of air parcel data are equal:"
+    cat x
+    rm x y
+else
+    echo "Number of air parcel data are not equal. C has: "
+    cat x
+    echo "and Fortran has: "
+    cat y
+    echo "Difference between var_atm_c.asc and var_atm_f.asc"
+    diff data/var_atm_c.asc data/var_atm_f.asc
     rm x y
     exit_code=99
     exit $exit_code
