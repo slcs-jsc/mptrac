cfd7b80b70b9b432b707dd6385b8c506bb121a29
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2024-12-27 09:59:40 +0100

    Added const qualifiers.

--- src/mptrac.c
+++ src/mptrac.c
@@ -2750,7 +2750,7 @@ void module_convection(
       const double rhotop = ptop / ttop;
 
       /* Get new density... */
-      double rho = rhobot + (rhotop - rhobot) * rs[ip];
+      const double rho = rhobot + (rhotop - rhobot) * rs[ip];
 
       /* Get pressure... */
       atm->p[ip] = LIN(rhobot, pbot, rhotop, ptop, rho);
@@ -3787,7 +3787,7 @@ void module_oh_chem(
       const double ki =
 	ctl->oh_chem[2] * (ctl->oh_chem[3] !=
 			   0 ? pow(298. / t, ctl->oh_chem[3]) : 1.);
-      double c = log10(k0 * M / ki);
+      const double c = log10(k0 * M / ki);
       k = k0 * M / (1. + k0 * M / ki) * pow(0.6, 1. / (1. + c * c));
     }
 
