0a523495b1b6d4ba5c14238e1f771d8ba003b60a
janhclem <clemens1@jwlogin02.juwels>
2025-02-27 10:16:17 +0100

    Corrected index...

--- src/mptrac.c
+++ src/mptrac.c
@@ -12379,7 +12379,7 @@ void dd_communicate_particles_cleo(
   printf("Test casting\n");
   unsigned int* sdgbx_index_ptr = (unsigned int*) particles[nparticles-1].q[1];
   unsigned int sdgbx_index = *sdgbx_index_ptr;     
-  send_buffers[ndestinations-1][0].q[1] = (double) sdgbx_index;
+  send_buffers[ndestinations-1][nparticles-1].q[1] = (double) sdgbx_index;
   printf("%d to %f\n", *sdgbx_index_ptr, send_buffers[ndestinations-1][nparticles-1].q[1]);
 
   /* Sending... */
