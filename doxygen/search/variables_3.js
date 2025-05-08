2d4fb44a4fac430eb5be46a53d1875110d1fa2e0
janhclem <janhclem@users.noreply.github.com>
2025-05-08 12:42:11 +0000

    deploy: 934fc611d556386cadb154549466a8c18bd9c88f

---
+++ doxygen/search/variables_3.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['diffusion_0',['diffusion',['../structctl__t.html#a727a42a9f7c8b7e01c713d5300767213',1,'ctl_t']]],
+  ['direction_1',['direction',['../structctl__t.html#a1a6d5b4003ebd8f9c77b3b45933779e6',1,'ctl_t']]],
+  ['dry_5fdepo_5fdp_2',['dry_depo_dp',['../structctl__t.html#a60da7ce4a7b610712585fde03773bf7f',1,'ctl_t']]],
+  ['dry_5fdepo_5fvdep_3',['dry_depo_vdep',['../structctl__t.html#acadcfbb95971c47b377fbd4443c68723',1,'ctl_t']]],
+  ['dt_4',['dt',['../structcache__t.html#a2ea05d5c51e9149cac55020d5c1b8c92',1,'cache_t']]],
+  ['dt_5fkpp_5',['dt_kpp',['../structctl__t.html#a603d7f5e599d149b57a67fea612ecb04',1,'ctl_t']]],
+  ['dt_5fmet_6',['dt_met',['../structctl__t.html#a2d7b8100a2949004420841d81bcc6ab4',1,'ctl_t']]],
+  ['dt_5fmod_7',['dt_mod',['../structctl__t.html#a718d3200f867d9018369f12f66464807',1,'ctl_t']]]
+];
