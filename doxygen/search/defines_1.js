3f80c29716a32af7fb96769fac2fbb36edd177d6
lars2015 <lars2015@users.noreply.github.com>
2025-07-19 15:35:39 +0000

    deploy: 7805ef8c1bc5203d411e0d148cc4bbcdde1cad54

---
+++ doxygen/search/defines_1.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['co3_0',['CO3',['../mptrac_8h.html#ae27556d9109b33480031038071c8b4b0',1,'mptrac.h']]],
+  ['cp_1',['CP',['../mptrac_8h.html#a6594e57d1fff186da0a254a3742dcff7',1,'mptrac.h']]],
+  ['cpd_2',['CPD',['../mptrac_8h.html#af48c8f42b5451a04703d55d62ea50a3e',1,'mptrac.h']]],
+  ['csza_3',['CSZA',['../mptrac_8h.html#af7d97769fe58e5913e640ffed1d36fbf',1,'mptrac.h']]],
+  ['ct_4',['CT',['../mptrac_8h.html#a7e463881fb9033a2fb65f3782d4e1c01',1,'mptrac.h']]],
+  ['cts_5',['CTS',['../mptrac_8h.html#a9d9ce8cebc572a42e7b4ab9e92c3a350',1,'mptrac.h']]],
+  ['cy_6',['CY',['../mptrac_8h.html#ac5bc792e372b15e7a1d3efe6daac9aec',1,'mptrac.h']]]
+];
