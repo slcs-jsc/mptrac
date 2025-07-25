a51f92c4f36b0a8485515c94dbbf5b145dae0737
lars2015 <lars2015@users.noreply.github.com>
2025-07-25 15:49:34 +0000

    deploy: 06b6089e101e5f67c094bd40a7d463b9c9d4b187

---
+++ doxygen/search/defines_11.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['z_0',['Z',['../mptrac_8h.html#a58b4f1573922a1f73dd4bf014fd441bf',1,'mptrac.h']]],
+  ['zdiff_1',['ZDIFF',['../mptrac_8h.html#a8c73522db5d14433bd9ad2bd45d0de5f',1,'mptrac.h']]],
+  ['zeta_2',['ZETA',['../mptrac_8h.html#a90ee947c906d48ce71affad259ac991d',1,'mptrac.h']]]
+];
---
+++ doxygen/search/variables_1.js
@@ -0,0 +1,16 @@
+var searchData=
+[
+  ['balloon_0',['balloon',['../structctl__t.html#a1aee054abc8c3fbaa1f6828f78f4e2f5',1,'ctl_t']]],
+  ['bound_5fdps_1',['bound_dps',['../structctl__t.html#a9e31767a1a873f2829a00cb15c0ae421',1,'ctl_t']]],
+  ['bound_5fdzs_2',['bound_dzs',['../structctl__t.html#a0c09fef6dfe4daa19ad717cd1e1b77ec',1,'ctl_t']]],
+  ['bound_5flat0_3',['bound_lat0',['../structctl__t.html#a686563c65fdd2d03d15cfdddf294584c',1,'ctl_t']]],
+  ['bound_5flat1_4',['bound_lat1',['../structctl__t.html#a3488ff97025f12fbe161d85b09ddc33e',1,'ctl_t']]],
+  ['bound_5fmass_5',['bound_mass',['../structctl__t.html#a25a01ebb0fcff6040d0003b88b13cdec',1,'ctl_t']]],
+  ['bound_5fmass_5ftrend_6',['bound_mass_trend',['../structctl__t.html#a9feaa00a9e7de02f8bd661d735b8b280',1,'ctl_t']]],
+  ['bound_5fp0_7',['bound_p0',['../structctl__t.html#aa1419e1cd3c485e89a5166c7e1f19caf',1,'ctl_t']]],
+  ['bound_5fp1_8',['bound_p1',['../structctl__t.html#a44cbf78eb4bbb11036922bc40faa2b4b',1,'ctl_t']]],
+  ['bound_5fpbl_9',['bound_pbl',['../structctl__t.html#a3e924116d2599fb79a734a74da11e8a2',1,'ctl_t']]],
+  ['bound_5fvmr_10',['bound_vmr',['../structctl__t.html#a4f7014dad580e4e745c910187468807c',1,'ctl_t']]],
+  ['bound_5fvmr_5ftrend_11',['bound_vmr_trend',['../structctl__t.html#a7a4b2ae4a45234a74df45b5adbe513cd',1,'ctl_t']]],
+  ['bound_5fzetas_12',['bound_zetas',['../structctl__t.html#ae7b26fcc7af034afe6144631986213ea',1,'ctl_t']]]
+];
