b4745c9bfca0e9a82e68b5f7ca9e5a513b33f195
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2025-10-30 18:00:54 +0100

    Revised code to calculate time steps.

--- src/met_check_dt.c
+++ src/met_check_dt.c
@@ -19,7 +19,7 @@
 
 /*! 
   \file
-  Check model time step based on given meeorological data.
+  Check model time step based on given meteorological data.
 */
 
 #include "mptrac.h"
@@ -37,8 +37,8 @@ int main(
   dd_t *dd;
 
   /* Check arguments... */
-  if (argc < 3)
-    ERRMSG("Give parameters: <ctl> <met>");
+  if (argc < 4)
+    ERRMSG("Give parameters: <ctl> <dt_file> <met>");
 
   /* Allocate... */
   ALLOC(clim, clim_t, 1);
@@ -47,71 +47,75 @@ int main(
 
   /* Read control parameters... */
   mptrac_read_ctl(argv[1], argc, argv, &ctl);
-  double kx = scan_ctl(argv[1], argc, argv, "KX", -1, "50.0", NULL);
-  double kz = scan_ctl(argv[1], argc, argv, "KZ", -1, "0.1", NULL);
+  const double kx = scan_ctl(argv[1], argc, argv, "KX", -1, "50.0", NULL);
+  const double kz = scan_ctl(argv[1], argc, argv, "KZ", -1, "0.1", NULL);
+  const double dx = 1e3 * scan_ctl(argv[1], argc, argv, "DX", -1, "", NULL);
+  const double c_max = scan_ctl(argv[1], argc, argv, "CMAX", -1, "0.5", NULL);
+  const double n_max = scan_ctl(argv[1], argc, argv, "NMAX", -1, "0.3", NULL);
 
   /* Read climatological data... */
   mptrac_read_clim(&ctl, clim);
 
   /* Read meteo data... */
-  if (!mptrac_read_met(argv[2], &ctl, clim, met, dd))
+  if (!mptrac_read_met(argv[3], &ctl, clim, met, dd))
     ERRMSG("Cannot open file!");
 
-  /* Set parameters... */
-  const double c_max = 1.0;
-  const double n = 0.3;
+  /* Create output file... */
+  FILE *out;
+  LOG(1, "Write time step data file: %s", argv[2]);
+  if (!(out = fopen(argv[2], "w")))
+    ERRMSG("Cannot create file!");
+
+  /* Write header... */
+  fprintf(out,
+	  "# $1 = height [km]\n"
+	  "# $2 = time step for horizontal advection [s]\n"
+	  "# $3 = time step for vertical advection [s]\n"
+	  "# $4 = time step for horizontal diffusion [s]\n"
+	  "# $5 = time step for vertical diffusion [s]\n\n");
 
   /* Loop over pressure levels... */
   for (int ip = 1; ip < met->np - 1; ip++) {
 
     /* Init... */
     double dt_x_min = 1e100;
-    double dt_y_min = 1e100;
     double dt_p_min = 1e100;
     double dt_dx_min = 1e100;
-    double dt_dy_min = 1e100;
     double dt_dp_min = 1e100;
 
     /* Loop over columns... */
-#pragma omp parallel for default(shared) collapse(2) reduction(min:dt_x_min,dt_y_min,dt_p_min,dt_dx_min,dt_dy_min,dt_dp_min)
+#pragma omp parallel for default(shared) collapse(2) reduction(min:dt_x_min,dt_p_min,dt_dx_min,dt_dp_min)
     for (int ix = 0; ix < met->nx; ix++)
       for (int iy = 1; iy < met->ny - 1; iy++) {
 
 	/* Check advection... */
-	const double dx =
-	  1e3 * fabs(DEG2DX(met->lon[1] - met->lon[0], met->lat[iy]));
-	const double dt_x = fabs(c_max * dx / met->u[ix][iy][ip]);
-	if (met->u[ix][iy][ip] != 0 && dt_x < dt_x_min)
-	  dt_x_min = dt_x;
-
-	const double dy =
-	  0.5 * 1e3 * fabs(DEG2DY(met->lat[iy + 1] - met->lat[iy - 1]));
-	const double dt_y = fabs(c_max * dy / met->v[ix][iy][ip]);
-	if (met->v[ix][iy][ip] != 0 && dt_y < dt_y_min)
-	  dt_y_min = dt_y;
+	const double vh =
+	  sqrt(SQR(met->u[ix][iy][ip]) + SQR(met->v[ix][iy][ip]));
+	const double dt_x = fabs(c_max * dx / vh);
+	if (vh != 0)
+	  dt_x_min = MIN(dt_x, dt_x_min);
 
 	const double dp = 0.5 * fabs(met->p[ip + 1] - met->p[ip - 1]);
 	const double dt_p = fabs(c_max * dp / met->w[ix][iy][ip]);
-	if (met->w[ix][iy][ip] != 0 && dt_p < dt_p_min)
-	  dt_p_min = dt_p;
+	if (met->w[ix][iy][ip])
+	  dt_p_min = MIN(dt_p, dt_p_min);
 
 	/* Check diffusion... */
-	const double dt_dx = 0.5 * SQR(n * dx) / kx;
-	if (dt_dx < dt_dx)
-	  dt_dx_min = dt_dx;
+	const double dt_dx = 0.5 * SQR(n_max * dx) / kx;
+	dt_dx_min = MIN(dt_dx, dt_dx_min);
 
-	const double dt_dy = 0.5 * SQR(n * dy) / kx;
-	if (dt_dy < dt_dy_min)
-	  dt_dy_min = dt_dy;
-
-	const double dt_dp = 0.5 * SQR(n * dp) / (SQR(met->p[ip] / H0 * 1e-3) * kz);
-	if (dt_dp < dt_dp_min)
-	  dt_dp_min = dt_dp;
+	const double dt_dp =
+	  0.5 * SQR(n_max * dp) / (SQR(met->p[ip] / (100. * H0)) * kz);
+	dt_dp_min = MIN(dt_dp, dt_dp_min);
       }
 
     /* Write output... */
-    printf("%g %g %g %g %g %g %g\n", met->p[ip], dt_x_min, dt_y_min, dt_p_min, dt_dx_min, dt_dy_min, dt_dp_min);
+    fprintf(out, "%g %g %g %g %g\n", Z(met->p[ip]), dt_x_min, dt_p_min,
+	    dt_dx_min, dt_dp_min);
   }
+
+  /* Close output file... */
+  fclose(out);
 
   /* Free... */
   free(clim);
