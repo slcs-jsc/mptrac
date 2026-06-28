1024d6d3508065c2c0783d7f3e8465ec339a4e49
lars2015 <lars2015@users.noreply.github.com>
2026-06-28 20:30:18 +0000

    deploy: c227207812f99f5299488c6535be51541a1f0072

--- doxygen/search/defines_1.js
+++ doxygen/search/defines_1.js
@@ -3,11 +3,16 @@ var searchData=
   ['clamp_0',['CLAMP',['../mptrac_8h.html#a49fc908b537b2bfd13462453f83267c0',1,'mptrac.h']]],
   ['co3_1',['CO3',['../mptrac_8h.html#ae27556d9109b33480031038071c8b4b0',1,'mptrac.h']]],
   ['codes_5fhandle_2',['codes_handle',['../mptrac_8h.html#a5aa74f0eae3fc2645b1fd1ec7efe0c8c',1,'mptrac.h']]],
-  ['compress_5fspeed_3',['COMPRESS_SPEED',['../mptrac_8h.html#ab1a8b004581fcbd915f4c8ab800f3b2c',1,'mptrac.h']]],
-  ['cp_4',['CP',['../mptrac_8h.html#a6594e57d1fff186da0a254a3742dcff7',1,'mptrac.h']]],
-  ['cpd_5',['CPD',['../mptrac_8h.html#af48c8f42b5451a04703d55d62ea50a3e',1,'mptrac.h']]],
-  ['csza_6',['CSZA',['../mptrac_8h.html#af7d97769fe58e5913e640ffed1d36fbf',1,'mptrac.h']]],
-  ['ct_7',['CT',['../mptrac_8h.html#a7e463881fb9033a2fb65f3782d4e1c01',1,'mptrac.h']]],
-  ['cts_8',['CTS',['../mptrac_8h.html#a9d9ce8cebc572a42e7b4ab9e92c3a350',1,'mptrac.h']]],
-  ['cy_9',['CY',['../mptrac_8h.html#ac5bc792e372b15e7a1d3efe6daac9aec',1,'mptrac.h']]]
+  ['compress_5fbpv_3',['COMPRESS_BPV',['../mptrac_8h.html#a1bd4533a462d5f016cf5bce30e135b91',1,'mptrac.h']]],
+  ['compress_5flog_5fread_4',['COMPRESS_LOG_READ',['../mptrac_8h.html#ae256d833da0bf1760dc841a61a0cbce5',1,'mptrac.h']]],
+  ['compress_5flog_5fwrite_5',['COMPRESS_LOG_WRITE',['../mptrac_8h.html#a55f7b79f627034376c01d57cb4459156',1,'mptrac.h']]],
+  ['compress_5flog_5fwrite_5fcomp_6',['COMPRESS_LOG_WRITE_COMP',['../mptrac_8h.html#abb90b698db1433a8023e2bf9979d1c9e',1,'mptrac.h']]],
+  ['compress_5fratio_7',['COMPRESS_RATIO',['../mptrac_8h.html#a79a66bb4e9ed84549b965dd0e446fe58',1,'mptrac.h']]],
+  ['compress_5fspeed_8',['COMPRESS_SPEED',['../mptrac_8h.html#ab1a8b004581fcbd915f4c8ab800f3b2c',1,'mptrac.h']]],
+  ['cp_9',['CP',['../mptrac_8h.html#a6594e57d1fff186da0a254a3742dcff7',1,'mptrac.h']]],
+  ['cpd_10',['CPD',['../mptrac_8h.html#af48c8f42b5451a04703d55d62ea50a3e',1,'mptrac.h']]],
+  ['csza_11',['CSZA',['../mptrac_8h.html#af7d97769fe58e5913e640ffed1d36fbf',1,'mptrac.h']]],
+  ['ct_12',['CT',['../mptrac_8h.html#a7e463881fb9033a2fb65f3782d4e1c01',1,'mptrac.h']]],
+  ['cts_13',['CTS',['../mptrac_8h.html#a9d9ce8cebc572a42e7b4ab9e92c3a350',1,'mptrac.h']]],
+  ['cy_14',['CY',['../mptrac_8h.html#ac5bc792e372b15e7a1d3efe6daac9aec',1,'mptrac.h']]]
 ];
